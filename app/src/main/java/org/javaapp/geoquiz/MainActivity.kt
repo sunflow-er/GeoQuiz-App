4b5004797072d7791b732b4d8ee4c9c1d90a37d4
sunflow-er <antjd03170@gmail.com>
2024-06-26 16:48:32 +0900

    MVC

--- app/src/main/java/org/javaapp/geoquiz/MainActivity.kt
+++ app/src/main/java/org/javaapp/geoquiz/MainActivity.kt
@@ -1,9 +1,13 @@
 package org.javaapp.geoquiz
 
 import android.app.ProgressDialog.show
+import android.media.Image
 import android.os.Bundle
 import android.view.Gravity
 import android.view.View
+import android.widget.Button
+import android.widget.ImageButton
+import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -13,13 +17,33 @@ import androidx.lifecycle.ReportFragment.Companion.reportFragment
 import org.javaapp.geoquiz.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
+    private lateinit var questionTextView: TextView
+    private lateinit var trueButton: Button
+    private lateinit var falseButton: Button
+    private lateinit var nextButton: ImageButton
+    private lateinit var prevButton: ImageButton
+
+    private val questionBank = listOf(
+        Question(R.string.question_australia, true),
+        Question(R.string.question_oceans, true),
+        Question(R.string.question_mideast, false),
+        Question(R.string.question_africa, false),
+        Question(R.string.question_americas, true),
+        Question(R.string.question_asia, true)
+    )
+
+    private var currentIndex = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
         enableEdgeToEdge()
-        setContentView(binding.root)
+        setContentView(R.layout.activity_main)
+
+        questionTextView = findViewById(R.id.question_text_view)
+        trueButton = findViewById(R.id.true_button)
+        falseButton = findViewById(R.id.false_button)
+        nextButton = findViewById(R.id.next_button)
+        prevButton = findViewById(R.id.prev_button)
 
         // 시스템UI로부터의 여백을 가져와 뷰의 패딩으로 설정, 시스템UI와 앱의 콘텐츠가 겹치지 않도록 한다.
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
@@ -28,16 +52,53 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
-        binding.trueButton.setOnClickListener { view: View ->
-            Toast.makeText(this@MainActivity, R.string.correct_toast, Toast.LENGTH_SHORT).show() // Activity는 Context의 하위 클래스이므로 this를 사용할 수 있다.
-
+        questionTextView.setOnClickListener {
+            currentIndex = (currentIndex + 1) % questionBank.size
+            updateQuestion()
         }
 
-        binding.falseButton.setOnClickListener(object : View.OnClickListener {
-            override fun onClick(v: View?) {
-                Toast.makeText(this@MainActivity, R.string.incorrect_toast, Toast.LENGTH_SHORT).show()
+        trueButton.setOnClickListener { view: View ->
+            checkAnswer(true)
+        }
 
+        falseButton.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                checkAnswer(false)
             }
         })
+
+        nextButton.setOnClickListener { view: View ->
+            currentIndex = (currentIndex + 1) % questionBank.size
+            updateQuestion()
+        }
+
+        prevButton.setOnClickListener {
+            currentIndex = if (currentIndex == 0) {
+                questionBank.size - 1
+            } else {
+                (currentIndex - 1) % questionBank.size
+            }
+            updateQuestion()
+        }
+
+        updateQuestion()
+    }
+
+    private fun updateQuestion() {
+        val questionTextResId = questionBank[currentIndex].textResId
+        questionTextView.setText(questionTextResId)
+    }
+
+    private fun checkAnswer(userAnswer: Boolean) {
+        val correctAnswer = questionBank[currentIndex].answer
+
+        val messageResId = if (userAnswer == correctAnswer) {
+            R.string.correct_toast
+        } else {
+            R.string.incorrect_toast
+        }
+
+        Toast.makeText(this, messageResId, Toast.LENGTH_SHORT)
+            .show() // Activity는 Context의 하위 클래스이므로 this를 사용할 수 있다.
     }
 }
---
+++ app/src/main/java/org/javaapp/geoquiz/Question.kt
@@ -0,0 +1,6 @@
+package org.javaapp.geoquiz
+
+import androidx.annotation.StringRes
+
+data class Question(@StringRes val textResId : Int, val answer : Boolean)
+
